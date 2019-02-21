7bc405c630f14e0c9f4920ee5a6d441298dfa255
WVC <wendy.canepi@ge.com>
2019-02-21 15:36:28 -0500

    almost done

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
+
+
+// $('#view').on('click', function (){
+//   
+//     for ( i=0; i<employeeList.length;i++) {
+//         $('.input-group-append').append(`<p>${employeeList[i].name}</p>`);
+//         $('.input-group-append').append(`<p>${employeeList[i].officeNum}</p>`);
+//         $('.input-group-append').append(`<p>${employeeList[i].phoneNum}</p>`);
+//       }
+// });
+
+
 const hideAll = function () {
     $('.pane').hide();
 }
+
 const search = function (fieldName, searchValue, action) {
     for (let i = 0; i < employeeList.length; i++) {
         if (employeeList[i][fieldName] === searchValue) {
@@ -11,39 +24,46 @@ const search = function (fieldName, searchValue, action) {
                     employeeList.splice(i, 1);
                     break;
             }
+
         }
     }
 }
+
 const showView = function () {
     hideAll();
     $('#viewPane').show();
-    
-    // Sets up the view pane
+
+    // Here is where I render the Employee view pane
     $('#viewPane').empty();
     for (let i = 0; i < employeeList.length; i++) {
         $('#viewPane').append(`<div class="record border border-dark m-3" id="record-${i}"></div>`);
         $(`#record-${i}`).append(`<p>Name: ${employeeList[i].name}</p>`);
-        $(`#record-${i}`).append(`<p>Office Phone: ${employeeList[i].officeNum}</p>`);
+        $(`#record-${i}`).append(`<p>Office Number: ${employeeList[i].officeNum}</p>`);
         $(`#record-${i}`).append(`<p>Phone Number: ${employeeList[i].phoneNum}</p>`);
     }
 }
+
 const showAdd = function () {
     hideAll();
     $('#addPane').show();
 }
+
 const showVerify = function () {
     hideAll();
     $('#verifyPane').show();
     $('#verifyAnswer').text('');
 }
+
 const showUpdate = function () {
     hideAll();
     $('#updatePane').show();
 }
+
 const showDelete = function () {
     hideAll();
     $('#deletePane').show();
 }
+
 const submitEmp = function () {
     let newEmp = {
         'name': $('[name~=addEmpName]').val(),
@@ -53,20 +73,26 @@ const submitEmp = function () {
     employeeList.push(newEmp);
     showView();
 }
+
 const verifyEmp = function () {
     let emp = $('[name~=verifyEmpName]').val();
     let verify = 'No';
     let searchResults = search('name', emp, 'verify');
+
     if (searchResults) {
         verify = 'Yes';
     }
+
     $('#verifyAnswer').text(verify);
 }
+
 const updateEmp = function () {
     let name = $('[name~=updateEmpName]').val();
     let office = $('[name~=updateEmpOfficeNum]').val();
     let phone = $('[name~=updateEmpPhoneNum]').val();
+
     let searchResults = search('name', name, 'verify');
+
     if (searchResults) {
         for (let i = 0; i < employeeList.length; i++) {
             if (employeeList[i].name === name) {
@@ -77,13 +103,17 @@ const updateEmp = function () {
     }
     showView();
 }
+
 const deleteEmp = function () {
     let emp = $('[name~=deleteEmpName]').val();
     search('name', emp, 'delete');
     showView();
 }
 
+
+// CallBacks
 showView();
+
 $('#view').on('click', function (){
     showView();
     $('a').removeClass('active');
